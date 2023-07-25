08e117249fcd421a3eba370119d0893a114ec4ef
Jonathanflesch <jonathanflesch10@gmail.com>
2023-07-25 08:12:14 -0300

    Primeira funcao

---
+++ jogo.py
@@ -0,0 +1,21 @@
+def transforma_base(lista):
+    if len(lista)==0:
+        return {}
+    faceis=[]
+    medio=[]
+    dificil=[]
+    final = dict()
+    for questao in lista:
+        if questao['nivel']=='facil':
+            faceis.append(questao)
+        elif questao['nivel']=='medio':
+            medio.append(questao)
+        else:
+            dificil.append(questao)
+    if len(faceis)>0:
+        final['facil']=faceis
+    if len(medio)>0:
+        final['medio']=medio
+    if len(dificil)>0:
+        final['dificil']=dificil
+    return final
